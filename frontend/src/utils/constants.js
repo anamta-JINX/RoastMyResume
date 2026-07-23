a55c2d6fbe34bf6a3e3eacf67f96ed2fe63f390c
Anamta Gohar <anamta.gohar25@gmail.com>
2026-07-23 13:12:00 +0500

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL
-    ?.replace(/\/$/, "") || "/api";
+  import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") || "/api";
 
 export const ROAST_LEVELS = [
   {
