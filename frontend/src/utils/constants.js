7aca8386ff1c6ee61a76db53ccf5054fb092a193
Anamta Gohar <anamta.gohar25@gmail.com>
2026-07-23 12:40:23 +0500

    Update API_BASE_URL to use environment variable

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL
+    ?.replace(/\/$/, "") || "/api";
 
 export const ROAST_LEVELS = [
   {
